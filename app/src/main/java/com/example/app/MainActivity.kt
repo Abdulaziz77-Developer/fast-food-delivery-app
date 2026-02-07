4b414afddfadcc0aa96a89b4e32de3a6b1af11a3
Abdulaziz77-Developer <mirboboevabdulaziz5@gmail.com>
2026-02-07 11:11:21 +0500

    implement second page UI

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -6,20 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,6 +37,7 @@ import com.example.app.ui.theme.AppTheme
 import com.example.app.ui.theme.YongFontFamily
 import com.example.app.ui.theme.startRed
 import com.example.app.ui.theme.testColor
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,17 +45,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    WavesOfFoodScreen()
-                }
+                MainScreen()
             }
         }
     }
 }
+@Composable
+fun MainScreen(){
+    var spleshScreen by remember {
+        mutableStateOf(true)
+    }
+    LaunchedEffect(Unit) {
+        delay(2000)
+        spleshScreen = false
+    }
+    if (spleshScreen){
+        WavesOfFoodScreen()
+    }else{
+        Homescreen()
+    }
+}
+
 
 @Composable
-
-@Preview(showBackground = true)
 fun WavesOfFoodScreen(){
     Column(
         modifier= Modifier
@@ -83,11 +106,78 @@ fun WavesOfFoodScreen(){
                 .height(300.dp)
         )
         Text(
-            text = "Design By \n" +
+            text = "Design By" +
                     "NeatRoots",
             color = startRed,
             fontFamily = YongFontFamily,
             fontSize = 18.sp
         )
     }
+}
+@Composable
+@Preview(showBackground = true)
+fun Homescreen()
+{
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(horizontal = 24 .dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Spacer(modifier = Modifier.height(90.dp))
+        Image(
+            painter = painterResource(id = R.drawable.backkgroundstart1),
+            contentDescription = "Wafes of food illustration",
+            modifier = Modifier
+                .size(333.dp)
+        )
+        Text(
+            modifier = Modifier.padding(top = 20.dp),
+            text = "Enjoy Restaurant Quality Meals at Home ",
+            fontFamily = YongFontFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            color = startRed,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(270.dp))
+        GradientButton("Next") {
+
+        }
+       Text(
+           text = "Design By" +
+                   "NeatRoots",
+           color = startRed,
+           fontFamily = YongFontFamily,
+           fontSize = 18.sp,
+           modifier = Modifier.padding(top = 16.dp),
+           fontWeight = FontWeight.Bold
+       )
+    }
+}
+
+@Composable
+fun GradientButton(text: String,
+            onClick: () -> Unit ) {
+      Box(
+          modifier = Modifier
+              .background(
+                  brush = Brush.horizontalGradient(
+                      colors = listOf(
+                          startRed,
+                          testColor
+                      )
+                  ),
+                  shape = RoundedCornerShape(10.dp)
+              )
+              .clickable {
+                  onClick()
+              }
+              .padding(vertical = 16.dp, horizontal = 24.dp),
+          contentAlignment = Alignment.Center
+      ){
+          Text(text = text, color = Color.White, fontSize = 18.sp, fontFamily = YongFontFamily)
+      }
 }
